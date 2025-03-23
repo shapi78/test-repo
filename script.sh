94ad771009d5d01e55c23f812edc95696b3f8017
shmador <dorattar4@gmail.com>
2025-03-23 15:38:11 +0200

    k

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 while true; do
-	CURRENT_HASH=$(git rev-parse --short HEAD)
+	CURRENT_HASH=$(git rev-parse HEAD)
 	LAST_HASH=$(cat file.txt)
 	if [[ "$CURRENT_HASH" == "$LAST_HASH" ]]; then
 		echo "New commit happened!"
