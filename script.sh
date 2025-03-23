daea7e9fd4e2832d3b9f4eb88466611ca6b72497
shmador <dorattar4@gmail.com>
2025-03-23 20:33:58 +0200

    Another tagging test

--- script.sh
+++ script.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
-version=1
+VERSION_FILE=version.txt
+if [ -s "$VERSION_FILE" ]; then
+	version=$(cat "$VERSION_FILE")
+else
+	version=1
+fi
+TAG="dor-v${version}.0"
 while true; do
 	CURRENT_HASH=$(git rev-parse HEAD)
 	LAST_HASH=$(cat file.txt)
 	if [[ "$CURRENT_HASH" != "$LAST_HASH" ]]; then
 		echo "New commit happened!"
-		git tag "$version"
-		git push origin "$version":dor
+		git tag "$TAG" &>/dev/null
+		git push origin "$TAG" &> /dev/null
 		version=$((version + 1))
 		CURRENT_HASH=$(git rev-parse HEAD)
 	else
 		echo "No new commits..."
 	fi
 	echo "$CURRENT_HASH" > file.txt
+	echo "$version" > "$VERSION_FILE"
 	sleep 3
 done
 
