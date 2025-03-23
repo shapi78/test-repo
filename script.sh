f2dbbc0798dcee94de6b1f0100824b0feae656f8
shmador <dorattar4@gmail.com>
2025-03-23 15:39:28 +0200

    s

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 while true; do
 	CURRENT_HASH=$(git rev-parse HEAD)
 	LAST_HASH=$(cat file.txt)
-	if [[ "$CURRENT_HASH" == "$LAST_HASH" ]]; then
+	if [[ "$CURRENT_HASH" != "$LAST_HASH" ]]; then
 		echo "New commit happened!"
 	else
 		echo "No new commits..."
