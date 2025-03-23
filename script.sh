9ee1d063fe39d1fee7916113aa4661042140d8d9
shmador <dorattar4@gmail.com>
2025-03-23 20:17:21 +0200

    Tagging test

--- script.sh
+++ script.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-
+version=1
 while true; do
 	CURRENT_HASH=$(git rev-parse HEAD)
 	LAST_HASH=$(cat file.txt)
 	if [[ "$CURRENT_HASH" != "$LAST_HASH" ]]; then
 		echo "New commit happened!"
+		git tag "$version"
+		git push origin "$version"	
+		version=$((version + 1))
+		CURRENT_HASH=$(git rev-parse HEAD)
 	else
 		echo "No new commits..."
 	fi
