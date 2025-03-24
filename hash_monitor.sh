2563cb87558bcab9640abedda030474dc6813802
Oleg, Braylovskiy <oleghoog123@gmail.com>
2025-03-24 12:15:36 +0200

    updated and squashed commits

---
+++ hash_monitor.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+repo_path="/home/oleg/Desktop/l_repo/test-repo"
+branch="olegb"
+version_file="version.txt"
+current_date=$(date +"%H:%M %d-%m-%y")
+
+if [ ! -f "$version_file" ]; then
+    echo "Version file does not exist, creating..."
+    touch $version_file
+fi
+
+cd $repo_path
+
+local_commit=$(git rev-parse HEAD)
+
+git fetch origin
+
+remote_commit=$(git rev-parse "origin/$branch")
+
+if [[ $local_commit != $remote_commit ]]; then
+    echo "There was a change"
+    echo "$current_date" >> "$version_file"
+    git add $version_file
+    git commit -m "Updated on: $current_date"
+    git push -f origin $branch
+    # git reset --hard HEAD
+else
+    echo "nothing changed"
+fi
