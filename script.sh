28897f7d9662a540a060787c760a6749fb952a0b
shmador <dorattar4@gmail.com>
2025-03-24 11:51:34 +0200

    Test

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 HASH_FILE=~/hashfile.txt
-VERSION_FILE=~/versionfile.txt
+VERSION_FILE_NAME="versionfile.txt"
+VERSION_FILE=~/${VERSION_FILE_NAME}
 
 while true; do
 	current_hash=$(git rev-parse HEAD)
@@ -11,8 +12,12 @@ while true; do
 		if [[ "$current_hash" != "$last_hash" ]]; then
 			echo "New commit with ${current_hash}"
 			git pull
-			current_hash=$(git rev-parse HEAD)
 			echo "$DATE" > "$VERSION_FILE"
+			cp "$VERSION_FILE" "$VERSION_FILE_NAME"
+			git add "$VERSION_FILE_NAME"
+			git commit -m "Updated ${DATE}"
+			git push	
+			current_hash=$(git rev-parse HEAD)
 		fi
 	fi
 	echo "$current_hash" > "$HASH_FILE"
