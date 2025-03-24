fc9c07e075f834192f537a2391da4bcd19a9ba8e
lironBinyamin <liron.binyamin96@gmail.com>
2025-03-24 11:05:52 +0200

    resolved conflict
    commit
    
    commit
    
    commit
    
    commit

--- git-auto-pull.sh
+++ git-auto-pull.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 REPO_PATH="/home/liron/Desktop/repo/test-repo/test-repo"
-BRANCH="main"
+BRANCH="liron"
+DATE=$(date +"%H:%M %d-%m-%y")
+VERSION_FILE=version.txt
 
 cd "$REPO_PATH" || exit
 
@@ -16,6 +18,15 @@ while true; do
 if [ "$LOCAL" != "$REMOTE" ]; then
         echo "A change was found in the repository...git pull."
         git pull origin "$BRANCH"
+	if [ ! -f "$VERSION_FILE" ]; then
+		echo "Last Commit: $DATE" > "$VERSION_FILE"
+	else
+		echo "Last commit: $DATE" >> "$VERSION_FILE"
+	fi
+	git add "$VERSION_FILE"
+        git commit -m "Auto-update version file: $DATE"
+        git push origin "$BRANCH"
+
 	exit 0
 else
 	echo "change not found"
