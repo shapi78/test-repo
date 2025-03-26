02e65c9dd6771cad1a8ce8b78cebad982ce60c54
mendelsh <mendelshperber@gmail.com>
2025-03-26 22:09:28 +0200

    # Please include a useful commit message!

--- tags-script.sh
+++ tags-script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+[[ "$1" == --blame=* ]] && blame_lines="${1#*=}" && shift || blame_lines=-1
+
 
 sorted_tags() {
     python3 -c "
@@ -29,48 +31,57 @@ print('\n'.join(sorted_arr))
 }
 
 
+file_border() {
+    local file="$1"
+    local i length=${#file}
+    local underline="_"
+    local overline="‾"  # u+203e
+    local top_border="" bottom_border="" spaces=""
+
+    for ((i=0; i<length+4; i++)); do
+        top_border+="$underline"
+        bottom_border+="$overline"
+        spaces+=" "
+    done
+
+    echo "                ${top_border}"
+    echo "               |${spaces}|"
+    echo "               |  $file  |"
+    echo "               |${spaces}|"
+    echo "                ${bottom_border}"
+}
+
+
 tags_diff_files() {
     echo "$(git diff --name-only $1 $2)"
 }
 
 
 echo_tags_files_difference() {
-    local i
-
     echo "              ======== Files Changes ========"
 
     for file in "$@"; do
-        length=${#file}
-        overline="‾"  # u+203e
-        underline="_"
-        spaces=""
+        [ "$blame_lines" -ne 0 ] && file_border "$file" || echo "   $file"
 
-        top_border=""
-        bottom_border=""
-        for ((i=0; i<length+4; i++)); do
-            top_border+="$underline"
-            bottom_border+="$overline"
-            spaces+=" "
-        done
+        if [ "$blame_lines" -ne 0 ]; then
+            echo "              --- Git Blame ---"
 
-        echo "                ${top_border}"
-        echo "               |${spaces}|"
-        echo "               |  $file  |"
-        echo "               |${spaces}|"
-        echo "                ${bottom_border}"
+            if [ "$blame_lines" -gt 0 ]; then
+                git blame "$tag1" -- "$file" | head -n "$blame_lines"
+            else
+                git blame "$tag1" -- "$file" || git blame "$tag2" -- "$file"
+            fi
 
-        echo "              --- Git Blame ---"
-
-        git blame "$tag1" -- "$file" || git blame "$tag2" -- "$file"
-
-        echo "------------------------------------------------"
+            echo "------------------------------------------------"
+        fi
     done
 }
 
 
 tags=($(sorted_tags "$(git tag | grep "^Rel")"))
-echo "Sorted Tags: ${tags[@]}"
-echo "Total tags: ${#tags[@]}"
+echo
+echo "Total Release: ${#tags[@]}"
+echo
 
 for ((i=0; i<${#tags[@]}-1; i++)); do
     tag1=${tags[$i]}
@@ -78,10 +89,10 @@ for ((i=0; i<${#tags[@]}-1; i++)); do
 
     diff_files=$(tags_diff_files "$tag1" "$tag2")
 
-    echo "======================   $tag1 -> $tag2   ========================="
+    echo "======================   $tag2 -> $tag1   ========================="
     echo
     if [ -z "$diff_files" ]; then
-        echo "No changes found between $tag1 and $tag2"
+        echo "No changes from $tag2 to $tag1"
     else
         echo_tags_files_difference $diff_files
     fi
