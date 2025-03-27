db6e4df106c75906a5984b0d7184c0a1c034c6f1
ofirgaash <ofirgaash@gmail.com>
2025-03-27 11:22:33 +0200

    123

--- ofirscript.sh
+++ ofirscript.sh
@@ -1,18 +1,18 @@
 #/bin/bash
 
 earlyVersion="$1"
-lateVersion="$2"
+lateVersion=$(git tag | tail -1)
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 2 ]; then
 	echo "you must specify three arguments:"
-	echo "for example    v1.1.5    v1.2.6   m(for messages) OR f(for files)"
+	echo "for example    v1.1.5   m(for messages) OR f(for files)"
 	exit 1
 fi
 
-if [[ $3 == "m" ]]; then
+if [[ $2 == "m" ]]; then
 	git log --pretty=format:"- %s" "$earlyVersion".."$lateVersion"
 fi
 
-if [[ $3 == "f" ]]; then
+if [[ $2 == "f" ]]; then
 	git diff --name-only "$earlyVersion" "$lateVersion"
 fi
